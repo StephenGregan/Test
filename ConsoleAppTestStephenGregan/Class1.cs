13c21690213372fc416cb15cfb80dc16b989b8e7
StephenGregan <grego@live.ie>
2018-06-05 14:47:44 +0100

    initial commit

---
+++ ConsoleAppTestStephenGregan/Class1.cs
@@ -0,0 +1,196 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace ConsoleAppTestStephenGregan
+//{
+    //    class Wrapper
+    //   {
+    //    [JsonProperty("JsonValues")]
+    //    public ValueSet ValueSet { get; set; }
+    //}
+
+    //class ValueSet
+    //{
+    //    [JsonProperty("id")]
+    //    public string Id { get; set; }
+    //    [JsonProperty("values")]
+    //    public Dictionary<string, Value> Values { get; set; }
+    //}
+
+    //class Value
+    //{
+    //    [JsonProperty("id")]
+    //    public string Id { get; set; }
+    //    [JsonProperty("diaplayName")]
+    //    public string DisplayName { get; set; }
+    //}
+
+    // To parse this JSON data, add NuGet 'Newtonsoft.Json' then do:
+    //
+    //    using ConsoleAppTestStephenGregan;
+    //
+    //    var welcome = Welcome.FromJson(jsonString);
+
+    namespace ConsoleAppTestStephenGregan
+    {
+        using System;
+        using System.Collections.Generic;
+
+        using System.Globalization;
+        using Newtonsoft.Json;
+        using Newtonsoft.Json.Converters;
+
+    class Wrapper
+    {
+        [JsonProperty("JsonWelcome")]
+        public Welcome Welcome { get; set; }
+    }
+
+    public partial class Welcome
+        {
+            [JsonProperty("id")]
+            public long Id { get; set; }
+
+            [JsonProperty("versionValue")]
+            public long VersionValue { get; set; }
+
+            [JsonProperty("uuid")]
+            public string Uuid { get; set; }
+
+            [JsonProperty("createdBy")]
+            public string CreatedBy { get; set; }
+
+            [JsonProperty("createdDate")]
+            public string CreatedDate { get; set; }
+
+            [JsonProperty("lastModifiedBy")]
+            public string LastModifiedBy { get; set; }
+
+            [JsonProperty("lastModifiedDate")]
+            public string LastModifiedDate { get; set; }
+
+            [JsonProperty("company.id")]
+            public long CompanyId { get; set; }
+
+            [JsonProperty("name")]
+            public string Name { get; set; }
+
+            [JsonProperty("displayName")]
+            public string DisplayName { get; set; }
+
+            [JsonProperty("salutation")]
+            public string Salutation { get; set; }
+
+            [JsonProperty("firstName")]
+            public string FirstName { get; set; }
+
+            [JsonProperty("middleName")]
+            public string MiddleName { get; set; }
+
+            [JsonProperty("lastName")]
+            public string LastName { get; set; }
+
+            [JsonProperty("nickName")]
+            public string NickName { get; set; }
+
+            [JsonProperty("suffix")]
+            public string Suffix { get; set; }
+
+            [JsonProperty("gender.id")]
+            public long GenderId { get; set; }
+
+            [JsonProperty("businessUnit.id")]
+            public string BusinessUnitId { get; set; }
+
+            [JsonProperty("dateOfBirth")]
+            public string DateOfBirth { get; set; }
+
+            [JsonProperty("contactTypes")]
+            public object[] ContactTypes { get; set; }
+
+            [JsonProperty("accountingReference")]
+            public string AccountingReference { get; set; }
+
+            [JsonProperty("referenceId")]
+            public string ReferenceId { get; set; }
+
+            [JsonProperty("languageMappings")]
+            public LanguageMapping[] LanguageMappings { get; set; }
+        }
+
+        public partial class LanguageMapping
+        {
+            [JsonProperty("id")]
+            public long Id { get; set; }
+
+            [JsonProperty("contact.id")]
+            public long ContactId { get; set; }
+
+            [JsonProperty("language.id")]
+            public long LanguageId { get; set; }
+
+            [JsonProperty("language")]
+            public Language Language { get; set; }
+        }
+
+        public partial class Language
+        {
+            [JsonProperty("id")]
+            public long Id { get; set; }
+
+            [JsonProperty("label")]
+            public string Label { get; set; }
+
+            [JsonProperty("description")]
+            public string Description { get; set; }
+
+            [JsonProperty("alternates")]
+            public object Alternates { get; set; }
+
+            [JsonProperty("value")]
+            public string Value { get; set; }
+
+            [JsonProperty("subtag")]
+            public string Subtag { get; set; }
+
+            [JsonProperty("iso639_3Tag")]
+            public string Iso6393Tag { get; set; }
+
+            [JsonProperty("type")]
+            public string Type { get; set; }
+
+            [JsonProperty("alias")]
+            public object Alias { get; set; }
+
+            [JsonProperty("enabled")]
+            public bool Enabled { get; set; }
+        }
+
+        public partial class Welcome
+        {
+            public static Welcome FromJson(string json) => JsonConvert.DeserializeObject<Welcome>(json, ConsoleAppTestStephenGregan.Converter.Settings);
+        }
+
+        public static class Serialize
+        {
+            public static string ToJson(this Welcome self) => JsonConvert.SerializeObject(self, ConsoleAppTestStephenGregan.Converter.Settings);
+        }
+
+        internal static class Converter
+        {
+            public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+            {
+                MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+                DateParseHandling = DateParseHandling.None,
+                Converters = {
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+            };
+        }
+    }
+
+
---
+++ ConsoleAppTestStephenGregan/Program.cs
@@ -0,0 +1,125 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTestStephenGregan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string json = @"[
+            //                  {
+            //                    'Title': 'Json.NET is awesome!',
+            //                    'Author': {
+            //                      'Name': 'James Newton-King',
+            //                      'Twitter': '@JamesNK',
+            //                      'Picture': '/jamesnk.png'
+            //                    },
+            //                    'Date': '2013-01-23T19:30:00',
+            //                    'BodyHtml': '&lt;h3&gt;Title!&lt;/h3&gt;\r\n&lt;p&gt;Content!&lt;/p&gt;'
+            //                  }
+            //                ]";
+
+            //dynamic blogPosts = JArray.Parse(json);
+
+            //dynamic blogPost = blogPosts[0];
+
+            //string title = blogPost.Title;
+
+            //Console.WriteLine(title);
+            //// Json.NET is awesome!
+
+            //string author = blogPost.Author.Name;
+
+            //Console.WriteLine(author);
+            //// James Newton-King
+
+            //DateTime postDate = blogPost.Date;
+
+            //Console.WriteLine(postDate);
+            //// 23/01/2013 7:30:00 p.m.
+
+            string newJsonString = @"{
+	""id"": 10197,
+
+    ""versionValue"": 4,
+	""uuid"": ""c17229d8-6be6-4e5f-8799-c0776bc30f2a"",
+	""createdBy"": ""GustavoCunha@translation.ie"",
+	""createdDate"": ""01/06/18 16:44"",
+	""lastModifiedBy"": ""GustavoCunha @translation.ie"",
+	""lastModifiedDate"": ""01/06/18 16:44"",
+	""company.id"": 1,
+	""name"": ""Lycia Rodrigues"",
+	""displayName"": ""Lycia Rodrigues (10197)"",
+	""salutation"": "",
+	""firstName"": ""Lycia"",
+	""middleName"": "",
+	""lastName"": ""Rodrigues"",
+	""nickName"": "",
+	""suffix"": "",
+	""gender.id"": 2,
+	""businessUnit.id"": "",
+	""dateOfBirth"": ""06/01/95"",
+	""contactTypes"": [],
+	""accountingReference"": ""10197"",
+	""referenceId"": ""10197"",
+	""languageMappings"": [{
+		""id"": 17832,
+		""contact.id"": 10197,
+		""language.id"": 137,
+		""language"": {
+			""id"": 137,
+			""label"": ""Portuguese"",
+			""description"": ""Portuguese"",
+			""alternates"": null,
+			""value"": ""pt"",
+			""subtag"": ""pt"",
+			""iso639_3Tag"": ""por"",
+			""type"": ""language"",
+			""alias"": null,
+			""enabled"": true
+
+        }";
+
+            var valueSet = JsonConvert.DeserializeObject<Welcome>(newJsonString);
+
+            Console.WriteLine("id: " + valueSet.Id);
+
+
+
+            //        string jsonString = @"
+            //    {
+            //        ""JsonValues"": {
+            //            ""id"": ""MyID"",
+            //            ""values"": {
+            //                ""value1"": {
+            //                    ""id"": ""100"",
+            //                    ""diaplayName"": ""MyValue1""
+            //                },
+            //                ""value2"": {
+            //                    ""id"": ""200"",
+            //                    ""diaplayName"": ""MyValue2""
+            //                }
+            //            }
+            //        }
+            //    }";
+
+            //        var valueSet = JsonConvert.DeserializeObject<Wrapper>(jsonString).ValueSet;
+
+            //        Console.WriteLine("id: " + valueSet.Id);
+            //        foreach (KeyValuePair<string, Value> kvp in valueSet.Values)
+            //        {
+            //            Console.WriteLine(kvp.Key + " id: " + kvp.Value.Id);
+            //            Console.WriteLine(kvp.Key + " name: " + kvp.Value.DisplayName);
+            //        }
+            //    }
+            //}
+        }
+    }
+}
+
